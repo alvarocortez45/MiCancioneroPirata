7a0cc4df8e903ea8335af4e96bf3df174871fe0f
alvarito45 <alvarocortez455@gmail.com>
2016-05-30 15:05:32 -0300

    Add a splash activity and all layouts's styles

--- app/src/main/java/com/example/alvaro/micancioneropirata/Principal.java
+++ app/src/main/java/com/example/alvaro/micancioneropirata/Principal.java
@@ -2,12 +2,18 @@ package com.example.alvaro.micancioneropirata;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.content.Intent;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Principal extends AppCompatActivity {
 
@@ -15,20 +21,23 @@ public class Principal extends AppCompatActivity {
     ListView lv;
     String[] titulo_cancion = {"No se compara", "Belgrano esta de fiesta y vos de luto", "Éclair v2.0/2.1", "Froyo v2.2",
             "Gingerbread v2.2", "Honeycomb v3.0/3.1"};
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
-        lv = (ListView)findViewById(R.id.list);
-        final Intent intent = new Intent(this,Visor.class);
-        lv.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,titulo_cancion));
-
+        lv = (ListView) findViewById(R.id.list);
+        final Intent intent = new Intent(this, Visor.class);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titulo_cancion));
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                intent.putExtra(EXTRA_NOMBRE,titulo_cancion[position]);
+                int position, long id) {
+                intent.putExtra(EXTRA_NOMBRE, titulo_cancion[position]);
                 startActivity(intent);
-            }
+                }
+
         });
+
     }
+
 }
